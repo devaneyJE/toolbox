ba56b4888d2115a672566bc4671a68e25655ce5f
devaneyJE <devaney.jonah@gmail.com>
2021-01-03 15:56:39 -0500

    reduce in-script dotfile additions with use of external dotfiles repo reference

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,11 +1,6 @@
 #! /bin/bash
 
 # conditions to start running setup
-#if [ $# -eq 0 ] 
-#then
-#	echo "Missing options! Use -h for help."
-#	exit 1
-#fi
 
 if [ $# == 1 ] && [ $1 == "-h" ]; then
 	echo -e "Syntax:\n./linux_setup.sh"
@@ -24,23 +19,15 @@ fi
 
 echo -e "\n     -\n    -\n   -\n  -\n -\n--------------------\nStarting Setup!\n--------------------\n -\n  -\n   -\n    -\n     -\n"
 
-#setting vars if tests passed
+#setting vars if tests pass
 su=$SUDO_USER
 homedir=/home/$su
 todo=$homedir/.setup_todo.txt
-echo -e "Manual Setup ToDo List:\n" >> $todo
-chown $su:$su $todo
 
-# installing software/acquiring repos
-if ! [ -x "$(command -v vim)" ]; then
-	apt-get update -qq
-	apt-get install -qq vim -y
-fi
-if ! [ -x "$(command -v git)" ]; then
-	apt-get update -qq
-	apt-get install -qq git -y
-fi
-echo -e "\n     -\n    -\n   -\n  -\n -\n--------------------\nSoftware installed!\n--------------------\n -\n  -\n   -\n    -\n     -\n"
+# installing packages/acquiring repos
+apt-get update -qq && apt-get install -qq vim git -y
+
+echo -e "\n     -\n    -\n   -\n  -\n -\n--------------------\nPackages installed!\n--------------------\n -\n  -\n   -\n    -\n     -\n"
 
 ## making repos dir and cloning dirs
 mkdir -p $homedir/{wlist,repos/{seclists,devaneyJE}}
@@ -49,62 +36,36 @@ mv $homedir/repos/seclists/{Discovery/Web-Content/{big.txt,directory-list-2.3-me
 tar -zxf $homedir/wlist/rockyou.txt.tar.gz -C $homedir/wlist/ && rm $homedir/wlist/rockyou.txt.tar.gz
 cp -r /media/sf_VMShare/{dotfiles,toolbox} $homedir/repos/devaneyJE/
 echo "- create and add ssh keys" >> $todo
-chown -R $su:$su $homedir/{wlist,repos}
 echo -e "\n     -\n    -\n   -\n  -\n -\n--------------------\nRepositories created!\n--------------------\n -\n  -\n   -\n    -\n     -\n"
 
 ## copying vimrc
 if [ -d $homedir/repos/devaneyJE/dotfiles ]; then
-	git clone --quiet https://github.com/VundleVim/Vundle.vim.git $homedir/.vim/bundle/Vundle.vim >/dev/null
+	git clone https://github.com/VundleVim/Vundle.vim.git $homedir/.vim/bundle/Vundle.vim 
 	chown $su:$su $homedir/.vim
 	cp $homedir/repos/devaneyJE/dotfiles/vimrc $homedir/.vimrc
 	vim +PluginInstall +qall
 else
 	echo -e "set nu\nset rnu\nset colo elflord" > $homedir/.vimrc	
 fi 
+
 echo -e "\n     -\n    -\n   -\n  -\n -\n--------------------\n$su .vimrc configured!\n--------------------\n -\n  -\n   -\n    -\n     -\n"
 
 # adding to bashrc
-echo " " >> $homedir/.bashrc
-echo " " >> $homedir/.bashrc
-echo " " >> $homedir/.bashrc
+cat $homedir/repos/dotfiles/bashrc_additions >> $homedir/.bashrc
+echo -e " \n \ncat $todo" >> $homedir/.bashrc
 
-## general
-echo "# user mods below" >> $homedir/.bashrc
-echo "cat $todo" >> $homedir/.bashrc
-echo 'export VISUAL=vim' >> $homedir/.bashrc
-echo 'export EDITOR=$VISUAL' >> $homedir/.bashrc
-echo " " >> $homedir/.bashrc
-
-## path edit
-echo "## adding dirs to path" >> $homedir/.bashrc
-
-if [ -d $homedir/scripts ]; then
-	echo 'export PATH="$HOME/scripts:$PATH"' >> $homedir/.bashrc
-else
-	mkdir $homedir/scripts
-	chown $su:$su $homedir/scripts
-	echo 'export PATH="$HOME/scripts:$PATH"' >> $homedir/.bashrc
-fi	
-
-if [ -d $homedir/repos/devaneyJE/toolbox ]; then
-	echo 'export PATH="$HOME/repos/devaneyJE/toolbox:$PATH"' >> $homedir/.bashrc
-fi
-
-if [ -d $homedir/go ]; then
-	echo 'export PATH="$HOME/go/bin:$PATH"' >> $homedir/.bashrc
-fi
-echo " " >> $homedir/.bashrc
-
-## aliases
-echo "## user aliases" >> $homedir/.bashrc 
-echo "alias vibashrc='vim $homedir/.bashrc'" >> $homedir/.bashrc
-echo 'alias sudos='sudo env PATH=$PATH'' >> $homedir/.bashrc
-echo " " >> $homedir/.bashrc
 echo -e "\n     -\n    -\n   -\n  -\n -\n--------------------\n$su .bashrc configured!\n--------------------\n -\n  -\n   -\n    -\n     -\n"
 
 # create manual todo list
+echo -e "Manual Setup ToDo List:\n" >> $todo
 echo "- add '$su ALL=(ALL) NOPASSWD: ALL' to sudoers file" >> $todo
 echo "- change terminal color settings" >> $todo
 echo "- remove todo list reference from .bashrc" >> $todo
+
 echo -e "\n     -\n    -\n   -\n  -\n -\n--------------------\nToDo List created!\n--------------------\n -\n  -\n   -\n    -\n     -\n"
+
+chown -hR $su:$su $homedir
+
+echo -e "\n     -\n    -\n   -\n  -\n -\n--------------------\n$homedir file ownership adjusted for $su!\n--------------------\n -\n  -\n   -\n    -\n     -\n"
+
 cat $todo
