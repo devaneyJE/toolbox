09a9bffe0cf3e474bd87dd3dc14914d6f7168030
devaneyJE <devaney.jonah@gmail.com>
2021-02-28 11:04:35 -0500

    add arch_packages install script

---
+++ arch_packages.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+pacman -S git \
+			base-devel \
+			bspwm \
+			sxhkd \
+			kitty \
+			alacritty \
+			tmux \
+			vim \
+			emacs \
+			go \
+			go-tools \
+			php \
+			python3 \
+			lua \
+			ruby \
+			cmus \
+			pavucontrol \
+			pulseaudio-equalizer \
+			feh \
+			gimp \
+			vlc \
+			qt5 \
+			phonon-qt5-vlc \
+			mariadb \
+			sqlite \
+			redis \
+			openssh \
+			samba \
+			dmenu \
+			rofi \
+			wget \
+			netctl \
+			wireless_tools \
+			wpa_supplicant \
+			bluez \
+			bluez-utils \
+			pulseaudio-bluetooth \
+			efibootmgr \
+			firefox \
+			firefox-i18n-en-us
+
